7084eb72d2e7fac5417c699fc65b9b95fdd8d56c
smart86029 <smart86029@gmail.com>
2020-06-24 15:54:06 +0800

    feat: add identity projects

---
+++ src/Salacia.Identity.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Salacia.Identity.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
